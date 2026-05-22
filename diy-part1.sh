08d34ac3a6ccbc8a504de12dde70d5f25e8feacd
Coumarin-26026 <thanh02062k2@gmail.com>
2026-05-22 12:47:31 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,5 +23,5 @@
  #echo 'src-git Modem https://github.com/4IceG/luci-app-modemband.git' >>feeds.conf.default
  #echo 'src-git SMS https://github.com/4IceG/luci-app-sms-tool.git' >>feeds.conf.default
  #echo 'src-git atinout https://github.com/4IceG/luci-app-atinout-mod.git' >>feeds.conf.default
-sed -i '$a src-git modem https://github.com/kiddin9/kwrt-packages.git' feeds.conf.default
+sed -i '$a src-git modem https://github.com/kiddin9/Kwrt.git' feeds.conf.default
 
